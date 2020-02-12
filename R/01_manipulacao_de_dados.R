ac04b76d3a2a72d954501b9973c02cc559674d32
amandanarcizo <amandanarcizo@id.uff.br>
2020-02-12 16:06:22 -0300

    Script da aula 12.02.2020 do tabelão atualizado

--- R/01_manipulacao_de_dados.R
+++ R/01_manipulacao_de_dados.R
@@ -76,3 +76,83 @@ sum(rich == 1)
 
 #para saber qual site que tem riqueza igual a 1
 which(rich == 1)
+
+## Juntando diferentes tabelas por meio de identificadores comuns
+envir$Sites
+summary(envir$Sites)
+
+#modificando tipos de variaveis --> sites ainda esta sendo entendida como uma variavel numerica e precisa ser transformada em variável categorica porque é o nome das áreas. Vamos confirmar como ele entende essa variável:
+class(envir$Sites)
+
+#interger significa que é um numero inteiro e não decimal, ou seja, variavel numérica e temos que transformar em categorica
+
+as.factor(envir$Sites)
+
+#se usarmos apenas as.factor, não fazemos a conversão, vamos então fazer uma atribuição
+envir$Sites <- as.factor(envir$Sites)
+coord$Sites <- as.factor(coord$Sites)
+
+#Juntando envir e coord
+envir.cord <- merge(x = envir, y = coord, by = "Sites")
+dim(envir)
+dim(coord)
+dim(envir.cord)
+
+## Transformando uma matrix espécie vs. área em uma tabela de dados
+#Agora, queremos transformar a nossa matriz de espécie vs. área em uma planilha que contenha cada observação em uma linha e cada variável em uma coluna. Cada observação é a abundância de uma espécie em uma determinada área. Para fazer essa transformação iremos usar a função gather() do pacote tidyr. Como temos 97 sites e 56 espécies, terminaremos com um objeto com 5432 linhas
+
+Sites <- envir$Sites
+length(Sites)
+
+n.sp <- nrow(splist)
+n.sp
+
+#criando tabela com cada especie em cada area especies em linhas -> comm é uma matrix de especies em colunas e sites em linhas e a função gather do pacote tidyr faz essa transposição retirando a primeira coluna que é o nome dos sites
+comm.df <- tidyr::gather(comm[, -1])
+dim(comm.df)
+head(comm.df)
+
+#Queremos alterar o nome das colunas de comm.df. Para isso, usaremos a função colnames().
+
+colnames(comm.df)
+colnames(comm.df) <-  c("TaxCode", "Abundance")
+colnames(comm.df)
+
+#Queremos agora adicionar a coluna Sites ao novo objeto. Vamos usar a função rep(). Esta função cria sequências. Vamos criar uma sequência de localidades, em que cada uma das 97 localidades se repete 56 vezes. A sequência deve ter também 5432 elementos
+
+seq.site <- rep(Sites, times = n.sp)
+seq.site
+length(seq.site)
+
+#agora sinalizamos que a coluna sites de comm.df é igual a seq.site
+comm.df$Sites <- seq.site
+head(comm.df)
+
+## Juntando todas as variáveis com comm.df
+#Como vimos na aula, as relações entre duas tabelas são sempre feitas par a par. Então, vamos juntar par a par as tabelas usando a função merge().
+
+comm.sp <- merge(x =comm.df, y =splist, by= "TaxCode")
+head(comm.sp)
+
+#juntando com traits --> adicionamos os dados de atributos das espécies à tabela de comunidade. Na tabela traits, a coluna que identifica as espécies é chamada Sp. Antes de fazer a junção, precisamos mudar o nome para bater com o nome da coluna em comm.sp que é TaxCode.
+
+names(traits)
+colnames(traits) [1] <- "TaxCode"
+
+comm.traits <- merge(comm.sp, traits, by = "TaxCode")
+head(comm.traits)
+
+## Finalmente, juntamos as variáveis ambientais (que aqui já contém as coordenadas) à tabela geral da comunidade por meio da coluna Sites.
+
+comm.total <- merge(comm.traits, envir.cord, by = "Sites")
+head(comm.total)
+
+## Por último, finalizamos nossa rotina de manipulação de dados exportando a planilha final modificada. Para isso, usamos a função write.csv().
+
+write.csv(x = comm.total,
+         file = "data/01_data_format_combined.csv",
+         row.names = FALSE)
+
+####----------------------------------- FIM DESSA BAGACEIRA --------------
+
+
